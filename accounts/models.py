1f8b2f28ae1a6981b536212d3c440951091f826d
Praneet-K <122003194@sastra.ac.in>
2021-07-17 13:32:34 +0530

    passwordchanged

--- accounts/models.py
+++ accounts/models.py
@@ -1,11 +1,13 @@
 from django.db import models
 from django.contrib.auth.models import User
-
+import datetime
+from django.utils import timezone
 
 class Profile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
     image = models.ImageField(default='default.jpeg', upload_to='profile_pics')
     phno =models.IntegerField(default=999999999)
+    dob = models.DateField(default=timezone.now)
     state = models.CharField(default='Andhra Pradesh',max_length=100)
     university = models.CharField(default='Sastra',max_length=100)
     sem = models.IntegerField(default=5)
